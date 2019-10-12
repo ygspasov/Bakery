b993bff6afffda76c97a176dbe740eaa1147bd0d
Yavor Spassov <ygspasov@gmail.com>
2019-10-12 14:47:30 +0300

    initial app commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <footer>
+    <div class="container">
+      <img :src="Logo" />
+      <div class="contact">
+        <div>Call us</div>
+        <div>4234-54345</div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+import Logo from "../assets/images/logo.png";
+export default {
+  data() {
+    return {
+      Logo
+    };
+  }
+};
+</script>
+
+<style scoped>
+footer {
+  margin-top: 130px;
+  background: #212121;
+  color: #f8ce7a;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 32px;
+  padding: 40px 0px;
+}
+
+footer .contact {
+  border-top: 1px solid #f8ce7a;
+  padding: 20px;
+}
+
+footer .container {
+  text-align: center;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="logo">
+        <img :src="Logo" alt />
+      </div>
+      <div class="nav">
+        <img :src="Menu" alt />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import Logo from "../assets/images/logo.png";
+import Menu from "../assets/images/menu.png";
+
+export default {
+  data() {
+    return {
+      Logo,
+      Menu
+    };
+  }
+};
+</script>
+
+<style scoped>
+header {
+  background: #703901;
+  padding: 30px 0px 10px 0px;
+}
+
+header .nav img {
+  width: 75px;
+  cursor: pointer;
+}
+
+header .side-drawer {
+  background: #fff;
+}
+
+header .container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+.nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+header .nav img {
+  background: #f8ce7a;
+}
+</style>
